db2f1ebbc68570c2849c4b725a48c43da392ef1b
DiegoGDP <Diego.Guerrero@alu.uclm.es>
2022-10-18 16:30:51 +0200

    Updated version of the simulation with correct images and constants file

--- src/graph.py
+++ src/graph.py
@@ -1,9 +1,10 @@
 import igraph as ig
-import cv2
 from PIL import Image, ImageOps
+from constants import RESIZE_VALUE, VERTEX_SIZE, VERTEX_FRAME_WIDTH, VERTEX_LABEL_SIZE,\
+    EDGE_WIDTH, MARGIN, IMG_NAME
 
 
-def create_graph(img_name, num_vert, edges, types, names):
+def create_graph(num_vert, edges, types, names):
     '''Generates an image containing the graph representation'''
 
     graph = ig.Graph(num_vert, edges)
@@ -14,15 +15,15 @@ def create_graph(img_name, num_vert, edges, types, names):
 
     ig.plot(
         graph,
-        target = img_name,
+        target = IMG_NAME,
         layout = lay,
-        vertex_size = 42,
+        vertex_size = VERTEX_SIZE,
         vertex_color = "lightblue",
-        vertex_frame_width = 4.0,
+        vertex_frame_width = VERTEX_FRAME_WIDTH,
         vertex_frame_color = "white",
         vertex_label = graph.vs["name"],
-        vertex_label_size = 1.0,
-        edge_width = 4,
+        vertex_label_size = VERTEX_LABEL_SIZE,
+        edge_width = EDGE_WIDTH,
         edge_color = "black",
     )
 
@@ -31,12 +32,30 @@ def create_graph(img_name, num_vert, edges, types, names):
     for vert in range(num_vert):
         node_coords.update({graph.vs["name"][vert]:lay.coords[vert]})
 
-    return img_name, node_coords
+    return node_coords
 
 
 def pad_img(img_name):
     '''Adds margins to a given image'''
 
     img = Image.open(img_name)
-    img_with_border = ImageOps.expand(img, border = 50, fill = 'white')
+    img_with_border = ImageOps.expand(img, border = MARGIN, fill = 'white')
     img_with_border.save(img_name)
+
+
+def resize_img(img_name):
+    '''Resizes a given image'''
+
+    img = Image.open(img_name)
+    img_resize = img.resize(RESIZE_VALUE)
+    img_resize.save(img_name)
+
+
+def preprocess_graph(img_name, num_vert, edges, types, names):
+    '''Preprocesses a graph to output an image'''
+
+    node_coords = create_graph(num_vert, edges, types, names)
+    pad_img(img_name)
+    resize_img(img_name)
+
+    return node_coords
--- src/simulation.py
+++ src/simulation.py
@@ -1,33 +1,28 @@
 import pygame
-from graph import create_graph, pad_img
+from graph import preprocess_graph
+from constants import IMG_NAME, OFFSET_LABEL_X_NAME, OFFSET_LABEL_X_TYPE,\
+    OFFSET_LABEL_Y_TYPE, OFFSET_LABEL_Y_NAME, WIDTH_DISPLAY, HEIGHT_DISPLAY, MARGIN_DISPLAY,\
+    IMG_SIZE, DEVIATION_FACTOR, WHITE_COLOR, BLACK_COLOR, CAPTION, HELVETICA_FONT_SIZE_NAME,\
+    HELVETICA_FONT_SIZE_TYPE
 
-
-WIDTH = 900
-HEIGHT = 900
-MARGIN = 48
-NODE_RAD = 21
-IMG_SIZE = 600
-DEVIATION_FACTOR = 20
-
-WHITE_COLOR = (255,255,255)
-
-CAPTION = "Simulación de la casa"
 
 NUM_VERT = 8
 EDGES = [[0, 1], [2, 3], [1, 2], [1, 3], [4, 2], [4, 5], [6, 7], [7, 5]]
 TYPES = ["H", "H", "P", "H", "H", "P", "H", "H"]
-NAMES = ["hab1", "hab2", "pas1", "hab3", "hab4", "pas2", "hab5", "hab6"]
+NAMES = ["hab1", "hab2", "pas1", "hab3", "hab4", "paaaaaa2", "hab5", "hab6"]
 
 
 class Simulation(object):
     '''Class for the house simulation'''
 
     def __init__(self, num_vert, edges, types, names):
-        img_name, node_coords = create_graph("house_graph.png",
-            num_vert, edges, types, names)
-        pad_img(img_name)
+        node_coords = preprocess_graph(IMG_NAME, num_vert, edges, types, names)
 
         self.node_coords = node_coords
+        self.name_type = {}
+
+        for i in range(len(types)):
+            self.name_type.update({names[i]:types[i]})
 
 
     def run(self):
@@ -35,19 +30,23 @@ class Simulation(object):
 
         pygame.init()
 
-        display_surface = pygame.display.set_mode((WIDTH, HEIGHT))
+        display_surface = pygame.display.set_mode((WIDTH_DISPLAY, HEIGHT_DISPLAY))
         display_surface.fill(WHITE_COLOR)
         pygame.display.set_caption(CAPTION)
 
-        center_img = pygame.image.load(r'pm.png')
+        center_img = pygame.image.load(r'images/BaseRoom.png')
         house_img = pygame.image.load(r'house_graph.png')
+
+        display_surface.blit(house_img, (0, 0))
+        pygame.draw.line(display_surface, BLACK_COLOR, (HEIGHT_DISPLAY,0),
+            (HEIGHT_DISPLAY,HEIGHT_DISPLAY), 5)
+
+        self.mark_centers(display_surface, center_img)
+        self.add_labels(display_surface)
 
         running = True
 
         while running:
-            display_surface.blit(house_img, (0, 0))
-
-            self.mark_centers(display_surface, center_img)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -63,13 +62,52 @@ class Simulation(object):
         for coords in self.node_coords.values():
             if coords[1] < max_row:
                 display_surface.blit(center_img,
-                    ((MARGIN + (IMG_SIZE/max_row) * coords[0]) - DEVIATION_FACTOR * coords[0],
-                    (MARGIN + (IMG_SIZE/max_col) * coords[1]) - DEVIATION_FACTOR * coords[1]))
+                    ((MARGIN_DISPLAY + (IMG_SIZE/max_row) * coords[0]) \
+                        - DEVIATION_FACTOR * coords[0],
+                    (MARGIN_DISPLAY + (IMG_SIZE/max_col) * coords[1]) \
+                        - DEVIATION_FACTOR * coords[1]))
             else:
                 if [coords[0],coords[1]] in self.node_coords.values():
                     display_surface.blit(center_img,
-                    ((MARGIN + (IMG_SIZE/max_row) * coords[0]) - DEVIATION_FACTOR * coords[0],
-                    (MARGIN + (IMG_SIZE/max_col) * coords[1]) - DEVIATION_FACTOR * coords[1]))
+                    ((MARGIN_DISPLAY + (IMG_SIZE/max_row) * coords[0]) \
+                        - DEVIATION_FACTOR * coords[0],
+                    (MARGIN_DISPLAY + (IMG_SIZE/max_col) * coords[1]) \
+                        - DEVIATION_FACTOR * coords[1]))
+
+
+    def add_labels(self, display_surface):
+        '''Adds the necessary labels to the simulation'''
+
+        max_row, max_col = self.get_max_row_col_coords()
+
+        for name, coords in self.node_coords.items():
+
+            # Type labels
+            helvetica_font = pygame.font.SysFont('helvetica', HELVETICA_FONT_SIZE_TYPE)
+            type_room = self.name_type[name]
+            type_label = helvetica_font.render(type_room, HELVETICA_FONT_SIZE_TYPE, BLACK_COLOR)
+
+            first_coord = (MARGIN_DISPLAY + (IMG_SIZE/max_row) * coords[0]) \
+                - DEVIATION_FACTOR * coords[0] + OFFSET_LABEL_X_TYPE
+            second_coord = (MARGIN_DISPLAY + (IMG_SIZE/max_col) * coords[1]) \
+                - DEVIATION_FACTOR * coords[1] + OFFSET_LABEL_Y_TYPE
+
+            display_surface.blit(type_label, (first_coord, second_coord))
+
+            # Name labels
+            if len(name) > 7:
+                name = name[:6] + '...'
+
+            helvetica_font = pygame.font.SysFont('helvetica', HELVETICA_FONT_SIZE_NAME)
+            name_label = helvetica_font.render(name, HELVETICA_FONT_SIZE_NAME, BLACK_COLOR)
+
+            first_coord = (MARGIN_DISPLAY + (IMG_SIZE/max_row) * coords[0]) \
+                - DEVIATION_FACTOR * coords[0] + OFFSET_LABEL_X_NAME
+            second_coord = (MARGIN_DISPLAY + (IMG_SIZE/max_col) * coords[1]) \
+                - DEVIATION_FACTOR * coords[1] + OFFSET_LABEL_Y_NAME
+
+            display_surface.blit(name_label, (first_coord, second_coord))
+
 
     def get_max_row_col_coords(self):
         '''Gets the number of rows and columns in the graph'''
