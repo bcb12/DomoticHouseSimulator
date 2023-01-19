25516b2bf2a518da2ee3a8e6a6fbed3e368d1068
Raúl Barba Rojas <raul.barba@alu.uclm.es>
2023-01-19 18:42:46 +0100

    Organized antlr4

--- src/antlr4/Main.py
+++ src/antlr4/Main.py
@@ -4,6 +4,12 @@ from House import House
 import LALR
 import sys
 import logging
+from antlr4 import CommonTokenStream
+from antlr4 import ParseTreeWalker
+from antlr4 import InputStream
+from DHSemanticGrammarLexer import DHSemanticGrammarLexer
+from DHSemanticGrammarListener import DHSemanticGrammarListener
+from DHSemanticGrammarParser import DHSemanticGrammarParser
 
 
 def simulate_house(house):
@@ -51,8 +57,16 @@ def main():
     if len(sys.argv) == 1:
         logging.error('No file was provided. Try to provide a text file for the lexer to work.')
         return
-    house = LALR.main(sys.argv[1])
-    simulate_house(house)
+
+    with open(sys.argv[1], 'r+', encoding = 'utf-8') as file:
+        # lexer
+        lexer = DHSemanticGrammarLexer(InputStream(file.read()))
+        stream = CommonTokenStream(lexer)
+        # parser
+        parser = DHSemanticGrammarParser(stream)
+        tree = parser.casa()
+        house = tree.data
+        simulate_house(house)
 
 
 if __name__ == '__main__':
